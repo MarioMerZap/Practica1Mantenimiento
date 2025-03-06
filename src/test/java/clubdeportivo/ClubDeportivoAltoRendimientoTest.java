8f578efa0bc90a13d204268f0b6b710ad696e0c9
MarioMerZap <mariomerzap@uma.es>
2025-03-06 12:11:36 +0100

    Ahora funciona

---
+++ src/test/java/clubdeportivo/ClubDeportivoAltoRendimientoTest.java
@@ -0,0 +1,16 @@
+package clubdeportivo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ClubDeportivoAltoRendimientoTest {
+    
+    @Test
+    public void ejemplo() {
+        
+    }
+
+
+    
+}
